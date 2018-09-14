7afcf555affa815798158c4de060050de632bf79
Gustavo Costa <gustavolegal7@gmail.com>
2018-09-14 13:23:55 -0400

    staless ejb

---
+++ dadStateless/src/java/ejb/ProdutoService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import java.util.List;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import modelo.IProdutoService;
+import modelo.Produto;
+
+@Stateless
+@Remote(IProdutoService.class)
+public class ProdutoService implements IProdutoService{
+
+    @Override
+    public void inserir(Produto p) {
+    }
+
+    @Override
+    public List<Produto> listar() {
+        return new List<Produto>();
+    }
+
+    @Override
+    public void remover(Produto p) {
+    }
+    
+}
---
+++ dadStateless/src/java/modelo/IProdutoService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+
+import java.util.List;
+
+/**
+ *
+ * @author Aluno
+ */
+public interface IProdutoService {
+    public void inserir(Produto p);
+    public List<Produto> listar();
+    public void remover(Produto p);
+}
